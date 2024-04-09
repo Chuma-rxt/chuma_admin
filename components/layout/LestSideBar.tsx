003f4ccbc4894f918849e844a23a58474da4473a
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-09 10:27:29 +0200

    leftsidebar

--- components/layout/LestSideBar.tsx
+++ components/layout/LestSideBar.tsx
@@ -1,8 +1,18 @@
+import { navLinks } from '@/lib/constants'
 import React from 'react'
 
 const LestSideBar = () => {
   return (
-    <div className='h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>LestSideBar</div>
+    <div className='h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>
+        <img src="/logo.png" alt='logo' width={150} height={70} />
+
+        <div className='flex flex-col gap-12'>
+            {navLinks.map((link) => (
+                <Link href={link.url} key={link.label}></Link>
+            ))}
+
+        </div>
+    </div>
   )
 }
 
