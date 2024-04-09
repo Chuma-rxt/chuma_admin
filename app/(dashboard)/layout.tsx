6c231de5c645c27ca1b2d826db7a61bc6171d880
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-09 09:23:21 +0200

    Two files of the component created

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-
 import { ClerkProvider } from '@clerk/nextjs';
+
+import LestSideBar from "@/components/layout/LestSideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <LeftSideBar />
+          {children}</body>
       </html>
     </ClerkProvider>
   );
---
+++ components/layout/LestSideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LestSideBar = () => {
+  return (
+    <div>LestSideBar</div>
+  )
+}
+
+export default LestSideBar
