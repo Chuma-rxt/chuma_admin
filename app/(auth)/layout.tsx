6da738fc0550538c67fedc5665ca0cb4b5ab4605
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-09 02:04:22 +0200

    my

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chuma - Admin Dashboard",
+  title: "Chuma - Admin Auth",
   description: "Admin dashboard to mannage Chuma's data",
 };
 
