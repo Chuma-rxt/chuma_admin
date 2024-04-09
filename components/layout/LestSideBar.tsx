445060b1a1b0f887edea0e081a6851af4736daf8
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-09 10:03:59 +0200

    adding constants

--- components/layout/LestSideBar.tsx
+++ components/layout/LestSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LestSideBar = () => {
   return (
-    <div>LestSideBar</div>
+    <div className='h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>LestSideBar</div>
   )
 }
 
---
+++ lib/constants.ts
@@ -0,0 +1,16 @@
+import {
+    LayoutDashboard,
+    Shapes,
+    ShoppingBag,
+    Tag,
+    UsersRound,
+} fron "lucide-react";
+
+export const navLinks = [
+    {
+        url: "/",
+        icon: <LayoutDashboard />,
+        label: "Dashboard",
+    },
+    
+]
