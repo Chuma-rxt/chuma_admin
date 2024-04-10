4950a24afe07f543f86dcad8178fb5bc83476567
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-10 09:13:52 +0200

    Collection form

--- components/collection/CollectionForm.tsx
+++ components/collection/CollectionForm.tsx
@@ -1,18 +1,65 @@
 "use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 import { Separator } from "../ui/separator"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
-    username: z.string().min(2).max(50),
-  })
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
+  image: z.string()
+})
 
 
 const CollectionForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      image: "",
+    },
+  })
+
+  function onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
   return (
     <div className="p-10">
-        <p className="text-heading2-bold">Create Collection</p>
-        <Separator className="bg-grey-1 mt-4 mb-7" />
+      <p className="text-heading2-bold">Create Collection</p>
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   )
 }
