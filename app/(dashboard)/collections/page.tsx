53c3326b3fbcfb16adc967d396b8d07faa1d5586
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-18 10:11:02 +0200

    E-commerce

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,4 +1,30 @@
+"use client"
+import { useEffect, useState } from "react"
+
 const Collections = () => {
+  const [loading, setLoading] = useState(true)
+  const [collections, setCollections] = useState([])
+
+  const getCollections = async () => {
+    try {
+      const res = await fetch("/api/collections", {
+        method: "GET",
+      })
+      const data = await res.json()
+      setCollections(data)
+      setLoading(false)
+    } catch (err) {
+      console.log("[collections_GET]", err)
+    }
+  }
+
+  useEffect(() => {
+    getCollections()
+  }, [])
+
+  console.log(collections)
+
+  
   return (
     <div>Collections</div>
   )
